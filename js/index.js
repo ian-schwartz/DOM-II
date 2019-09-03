5de46768afa2994919bc50657f01c642704bdfde
Ian Schwartz <ian.schwartz23@gmail.com>
2019-09-03 13:03:01 -0700

    50% Complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+// 1) Mouseover
+const logoScale = document.querySelector('.logo-heading');
+
+logoScale.addEventListener('mouseover', e => {
+    logoScale.style.transform = 'scale(1.2)';
+});
+// 2) Click
+const headerChange = document.querySelector('.main-navigation');
+
+headerChange.addEventListener('click', e => {
+    headerChange.style.background = 'lightblue';
+});
+// 3) Dblclick
+const imgSwitch = document.querySelector('.intro-img');
+
+imgSwitch.addEventListener('dblclick', e => {
+    imgSwitch.src = 'img/destination.jpg';
+});
+// 4) Resize
+window.addEventListener('resize', e => {
+    const bgChange = document.querySelector('.content-section');
+    bgChange.style.background = 'lightblue';
+})
+// 5) Keydown
+window.addEventListener('keyup', e => {
+    const headingZoom = document.querySelector('.intro-heading');
+    headingZoom.style.fontSize = '7rem';
+});
+
